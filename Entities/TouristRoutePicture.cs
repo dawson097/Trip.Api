dd7234c553e55b0054d6833abcc2bcdf5abe09d3
dawson <dawson09@163.com>
2024-06-30 17:52:09 +0800

    Create data entity

---
+++ Entities/TouristRoutePicture.cs
@@ -0,0 +1,15 @@
+﻿namespace Trip.Api.Entities;
+
+/// <summary>
+/// 旅游路线图片实体
+/// </summary>
+public class TouristRoutePicture
+{
+    public int Id { get; set; }
+
+    public string Url { get; set; }
+
+    public Guid TouristRouteId { get; set; }
+
+    public TouristRoute TouristRoute { get; set; }
+}
