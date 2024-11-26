be1138dea125e1088efb9a1a08e2ab1534fa3cdc
Dawson <dawson09@163.com>
2024-11-26 22:48:50 +0800

    Add tourist route controller

---
+++ Controllers/TouristRoutesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trip.Api.Service.Interfaces;
+
+namespace Trip.Api.Controllers;
+
+[Route("api/tourist-routes")]
+[ApiController]
+public class TouristRoutesController : ControllerBase
+{
+    private readonly ITouristRouteRepository _routeRepository;
+
+    public TouristRoutesController(ITouristRouteRepository routeRepository)
+    {
+        _routeRepository = routeRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetTouristRoutes()
+    {
+        return Ok(_routeRepository.GetAllRoutes());
+    }
+
+    [HttpGet("{routeId:guid}")]
+    public IActionResult GetTouristRoute(Guid routeId)
+    {
+        return Ok(_routeRepository.GetRouteByRouteId(routeId));
+    }
+}
