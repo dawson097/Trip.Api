3df896c9338c91cbcbf2c705929f7a8ab9840e19
Dawson <dawson09@163.com>
2024-11-26 22:42:38 +0800

    Add data seeding

--- Migrations/20241126144036_AddDataSeeding.Designer.cs
+++ Migrations/20241126144036_AddDataSeeding.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Trip.Api.Database;
@@ -11,9 +12,11 @@ using Trip.Api.Database;
 namespace Trip.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241126144036_AddDataSeeding")]
+    partial class AddDataSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
