16765a21785057241fd00c81d4f231306a90f2c6
Dawson <dawson09@163.com>
2024-11-26 22:16:34 +0800

    Initialize database

--- Models/TouristRoutePicture.cs
+++ Models/TouristRoutePicture.cs
@@ -1,6 +1,21 @@
-﻿namespace Trip.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Trip.Api.Models;
+
+/// <summary>
+/// Data model of tourist route picture
+/// </summary>
 public class TouristRoutePicture
 {
-    
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string Url { get; set; }
+
+    [ForeignKey("TouristRouteId")]
+    public Guid TouristRouteId { get; set; }
+
+    public TouristRoute TouristRoute { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Trip.Api.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("NgSql");
+
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
